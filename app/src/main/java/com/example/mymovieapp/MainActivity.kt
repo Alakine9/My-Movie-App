1424c94e133542bdda0d47e96b851e0c5fc064f9
Alakine9 <wtrbflacka@gmail.com>
2025-11-06 09:37:33 +0000

    added changes on the code

--- app/src/main/java/com/example/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -18,3 +18,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+android:layout_width="match_parent"
+android:layout_height="match_parent"
+android:background="@android:color/white">
+
+<!-- Logo Image -->
+<ImageView
+android:id="@+id/logo"
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:src="@drawable/logo"  <!-- Use your logo image here -->
+android:layout_centerInParent="true" />
+
+</RelativeLayout>
+
+package com.example.movieapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    // Parallel Arrays for Movie Details
+    private val movieNames = arrayOf("Inception", "Titanic", "Avatar")
+    private val directors = arrayOf("Christopher Nolan", "James Cameron", "James Cameron")
+    private val ratings = arrayOf(8.8, 7.8, 7.8)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Buttons for Add Movie and View Reviews
+        val addMovieButton: Button = findViewById(R.id.addMovieButton)
+        val viewReviewsButton: Button = findViewById(R.id.viewReviewsButton)
+
+        // OnClickListener for Add Movie Button
+        addMovieButton.setOnClickListener {
+            val intent = Intent(this, AddMovieActivity::class.java)
+            startActivity(intent)
+        }
+
+        // OnClickListener for View Reviews Button
+        viewReviewsButton.setOnClickListener {
+            val intent = Intent(this, ViewReviewsActivity::class.java)
+            intent.putExtra("movieNames", movieNames)
+            intent.putExtra("directors", directors)
+            intent.putExtra("ratings", ratings)
+            startActivity(intent)
+        }
+    }
+}
